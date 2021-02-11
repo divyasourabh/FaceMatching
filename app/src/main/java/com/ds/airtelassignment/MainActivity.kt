2ffaff3a0c6ecadebf83df6f992f50f01469c6d1
divyasourabh <divyasourabh@gmail.com>
2021-02-11 19:44:37 +0530

    Develop UI

--- app/src/main/java/com/ds/airtelassignment/MainActivity.kt
+++ app/src/main/java/com/ds/airtelassignment/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.ds.airtelassignment
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var isFirstImageAdded = false
+    var isSecondImageAdded = false
+    var isFirstImage = false
+    var isFirstImageFaceFound=false
+    var isSecondImageFaceFound=false
+    var firstImageBitmap: Bitmap? = null
+    var secondImageBitmap: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
